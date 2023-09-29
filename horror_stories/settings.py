3d325d07514175a072c8abd0820965f6a793b28b
OSCAR-CASALS <138677821+OSCAR-CASALS@users.noreply.github.com>
2023-09-29 11:24:58 +0200

    Update settings.py

--- horror_stories/settings.py
+++ horror_stories/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a@pni5!+f@o@z@0#u8*-oj^3)gifn*7b2doiy8j4#&f%z3($g)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -88,11 +88,11 @@ DATABASES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'horror_stories',#database name
-        'USER': 'luckysuperior',#projectuser
-        'PASSWORD': 'Lucky123',#password
-        'HOST': '*',
-        'PORT': '5432',
+        'NAME': '****',#database name
+        'USER': '****',#projectuser
+        'PASSWORD': '****',#password
+        'HOST': '****',
+        'PORT': '****',
     }
 }
 
